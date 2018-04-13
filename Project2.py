0c4ecbc1fdfe186adf510694f6f9d6565e34ad66
CampusUser <campususer@CSUFMAC-011.fios-router.home>
2018-04-13 01:19:14 -0700

    Naive Bayes Working

--- Project2.py
+++ Project2.py
@@ -1,22 +1,39 @@
+'''
+
+References:
+    https://www.kaggle.com/startupsci/titanic-data-science-solutions
+    https://www.kaggle.com/minsukheo/titanic-solution-with-sklearn-classifiers
+    https://blog.sicara.com/naive-bayes-classifier-sklearn-python-example-tips-42d100429e44
+
+'''
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import cross_val_score, cross_val_predict
+
 
 def bar_chart(feature):
     survived = original[original['Income']==1][feature].value_counts()
     dead = original[original['Income']==0][feature].value_counts()
     df = pd.DataFrame([survived,dead])
-    df.index = ['Survived','Dead']
+    df.index = ['>50','<=50']
     df.plot(kind='bar',stacked=True, figsize=(10,5))
+    plt.show()
 
-
-''' Retrieve and fix data '''
+''' Retrieve and Fix data '''
 original = pd.read_csv('Dataset.csv', header=None, index_col=None)
 original.columns = ['Age', 'Work', 'Edu-Lvl', 'Edu-Years', 'Marriage-Status', 'Occupation', 'Relationship', 'Gender',
                     'Cap-Gain', 'Cap-Loss', 'Hours', 'Income']
+X_columns = ['Age', 'Work', 'Edu-Lvl', 'Edu-Years', 'Marriage-Status', 'Occupation', 'Relationship', 'Gender',
+                    'Cap-Gain', 'Cap-Loss', 'Hours']
+Y_columns = ['Income']
+
+# Number of empty values
+# print(original.isnull().sum())
+# print(original.describe(include="all"))
 
 original.replace(["?", "? ", " ?", " ? "], np.nan, inplace=True)
 original.replace(" <=50K", 0, inplace=True)
@@ -24,40 +41,91 @@ original.replace(" >50K", 1, inplace=True)
 # creating new column instead of replace
 # original["Income_cleaned"]=original["Income"].astype('category')
 # original["Income_cleaned"]=original["Income_cleaned"].cat.codes
+# # Drop null values
 original.dropna()
 
 
+''' Certain Columns can be grouped into ranges for easier analysis 
+    Grouping: Age, Edu-Years, Cap-Gain, Cap-Loss, Hours '''
+# Combine Age, Cap-Gain, Cap-Loss, Hours
+original.loc[original.Age <= 21, 'Age'] = 0
+original.loc[(original.Age > 21) & (original.Age <= 30), 'Age'] = 1
+original.loc[(original.Age > 30) & (original.Age <= 50), 'Age'] = 2
+original.loc[(original.Age > 50) & (original.Age <= 70), 'Age'] = 3
+original.loc[original.Age > 70, 'Age'] = 4
+
+original.loc[original['Cap-Gain'] <= 2000, 'Cap-Gain'] = 0
+original.loc[(original['Cap-Gain'] > 2000) & (original['Cap-Gain'] <= 4000), 'Cap-Gain'] = 1
+original.loc[(original['Cap-Gain'] > 4000) & (original['Cap-Gain'] <= 6000), 'Cap-Gain'] = 2
+original.loc[(original['Cap-Gain'] > 6000) & (original['Cap-Gain'] <= 10000), 'Cap-Gain'] = 3
+original.loc[original['Cap-Gain'] > 10000, 'Cap-Gain'] = 4
+
+original.loc[original['Cap-Loss'] <= 1300, 'Cap-Loss'] = 0
+original.loc[(original['Cap-Loss'] > 1300) & (original['Cap-Loss'] <= 1600), 'Cap-Loss'] = 1
+original.loc[(original['Cap-Loss'] > 1600) & (original['Cap-Loss'] <= 1900), 'Cap-Loss'] = 2
+original.loc[(original['Cap-Loss'] > 1900) & (original['Cap-Loss'] <= 2200), 'Cap-Loss'] = 3
+original.loc[original['Cap-Loss'] > 2200, 'Cap-Loss'] = 4
+
+original.loc[original.Hours <= 20, 'Hours'] = 0
+original.loc[(original.Hours > 20) & (original.Hours <= 40), 'Hours'] = 1
+original.loc[(original.Hours > 40) & (original.Hours <= 60), 'Hours'] = 2
+original.loc[(original.Hours > 60) & (original.Hours <= 80), 'Hours'] = 3
+original.loc[original.Hours > 80, 'Hours'] = 4
+
+
 ''' Analyze Data '''
-# print(original)
-print(original.head(5))
+print(original.head(10))
 # print(original.columns.values)
 # print("Data shape", original.shape)
 
-# Info on numerical and categorical values
+# #Info on numerical and categorical values
 # print(original.info())
 # print(original.describe(include=['O']))
 # print(original.describe())
 
-# Number of empty values
-# print(original.isnull().sum())
-# print(original.head(80))
-# print(original.describe(include="all"))
+''' Look at percentage fo each category where Income >50K'''
+# print(original[['Work', 'Income']].groupby(['Work'], as_index=False).mean().sort_values(by='Income', ascending=False) )
+# print(original[['Edu-Lvl', 'Income']].groupby(['Edu-Lvl'], as_index=False).mean().sort_values(by='Income', ascending=False) )
+# print(original[['Marriage-Status', 'Income']].groupby(['Marriage-Status'], as_index=False).mean().sort_values(by='Income', ascending=False) )
+# print(original[['Occupation', 'Income']].groupby(['Occupation'], as_index=False).mean().sort_values(by='Income', ascending=False) )
+# print(original[['Relationship', 'Income']].groupby(['Relationship'], as_index=False).mean().sort_values(by='Income', ascending=False) )
+# print(original[['Gender', 'Income']].groupby(['Gender'], as_index=False).mean().sort_values(by='Income', ascending=False) )
 
-# original["Income_cleaned"]=original["Income"].astype('category')
-# original["Income_cleaned"]=original["Income_cleaned"].cat.codes
+# print(original[['Age', 'Income']].groupby(['Age'], as_index=False).mean().sort_values(by='Income', ascending=False) )
+# print(original[['Edu-Years', 'Income']].groupby(['Edu-Years'], as_index=False).mean().sort_values(by='Income', ascending=False) )
+# print(original[['Cap-Gain', 'Income']].groupby(['Cap-Gain'], as_index=False).mean().sort_values(by='Income', ascending=False) )
+# print(original[['Cap-Loss', 'Income']].groupby(['Cap-Loss'], as_index=False).mean().sort_values(by='Income', ascending=False) )
+# print(original[['Hours', 'Income']].groupby(['Hours'], as_index=False).mean().sort_values(by='Income', ascending=False) )
 
-
-# print(original[['Work', 'Income_cleaned']].groupby(['Work'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-# print(original[['Edu-Lvl', 'Income_cleaned']].groupby(['Edu-Lvl'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-# print(original[['Marriage-Status', 'Income_cleaned']].groupby(['Marriage-Status'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-# print(original[['Occupation', 'Income_cleaned']].groupby(['Occupation'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-# print(original[['Relationship', 'Income_cleaned']].groupby(['Relationship'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-# print(original[['Gender', 'Income_cleaned']].groupby(['Gender'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-
-# print(original[['Age', 'Income_cleaned']].groupby(['Age'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-# print(original[['Edu-Years', 'Income_cleaned']].groupby(['Edu-Years'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-# print(original[['Cap-Gain', 'Income_cleaned']].groupby(['Cap-Gain'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-# print(original[['Cap-Loss', 'Income_cleaned']].groupby(['Cap-Loss'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
-# print(original[['Hours', 'Income_cleaned']].groupby(['Hours'], as_index=False).mean().sort_values(by='Income_cleaned', ascending=False) )
+# Bar chart distributions
+bar_chart('Edu-Years')
+bar_chart('Age')
 
 
+''' Convert Category Columns to numerical '''
+original["Work"]=original["Work"].astype('category')
+original["Work"]=original["Work"].cat.codes
+
+original["Edu-Lvl"]=original["Edu-Lvl"].astype('category')
+original["Edu-Lvl"]=original["Edu-Lvl"].cat.codes
+
+original["Marriage-Status"]=original["Marriage-Status"].astype('category')
+original["Marriage-Status"]=original["Marriage-Status"].cat.codes
+
+original["Occupation"]=original["Occupation"].astype('category')
+original["Occupation"]=original["Occupation"].cat.codes
+
+original["Relationship"]=original["Relationship"].astype('category')
+original["Relationship"]=original["Relationship"].cat.codes
+
+original["Gender"]=original["Gender"].astype('category')
+original["Gender"]=original["Gender"].cat.codes
+# print(original.head(10))
+
+
+''' Naive Bayes '''
+nbModel = GaussianNB()
+nbModel.fit(original[X_columns], original['Income'])
+score = cross_val_score(nbModel, original[X_columns], original['Income'], cv=10)
+print(score)
+print(round(np.mean(score)*100, 2) )
